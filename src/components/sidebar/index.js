71c8faff7a878281aa37839568d138d7fbf399eb
Jason Manning <jasonkylemanning@gmail.com>
2022-04-04 12:37:57 -0400

    add nav icons/routes

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,12 +1,33 @@
-import { Link } from 'react-router-dom'
-import './index.scss'
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
 import LogoJ from '../../assets/images/logo-j.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoJ} alt='logo' />
         </Link>
+        <nav>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/sirjkm/'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
